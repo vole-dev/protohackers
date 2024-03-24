dac52dd2db775a9ed0e6b02016c5c151becbb1db
vole-dev <dev@vole.dev>
2024-03-24 19:46:05 +0000

    00-smoke-test

---
+++ 00-smoke-test.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":1500")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatal(err)
+		}
+		go handleConnection(conn)
+	}
+}
+
+func handleConnection(conn net.Conn) {
+	buffer := make([]byte, 1024)
+	for {
+		length, err := conn.Read(buffer)
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Fatal(err)
+		}
+		conn.Write(buffer[0:length])
+	}
+	err := conn.Close()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
